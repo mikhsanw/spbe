c3a34a4316d9d861747e9fa72af7f22b80c8f39b
M. Ikhsan Wibowo <mikhsanw@riau.go.id>
2023-02-23 14:30:38 +0700

    tambah level admin dan operator di config

--- config/master.php
+++ config/master.php
@@ -26,6 +26,8 @@ return [
     'level' => [
                     0 => 'Unknown',
                     1 => 'Root',
+                    2 => 'Admin',
+                    3 => 'Operator',
     ],
     'url'   =>  [
                     'admin'     => '',
